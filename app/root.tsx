8f85ed7a02efe9324d4a067bd214b4a39220c5d8
Assaf Arkin <assaf@labnotes.org>
2025-06-11 19:56:29 -0700

    Added nav links in footer

--- app/root.tsx
+++ app/root.tsx
@@ -1,57 +1,63 @@
 import {
-	Links,
-	type LinksFunction,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
+  Links,
+  type LinksFunction,
+  Meta,
+  NavLink,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from "react-router";
 import "./app.css";
 
 export const links: LinksFunction = () => [
-	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-	{
-		rel: "preconnect",
-		href: "https://fonts.gstatic.com",
-		crossOrigin: "anonymous",
-	},
-	{
-		rel: "stylesheet",
-		href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-	},
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Meta />
-				<Links />
-			</head>
-			<body>
-				{children}
-				<Footer />
-				<ScrollRestoration />
-				<Scripts />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <Footer />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
 }
 
 function Footer() {
-	return (
-		<footer>
-			<div className="container mx-auto p-4 text-center">
-				<p className="text-gray-600">
-					© {new Date().getFullYear()} rentail.space. All rights reserved.
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer>
+      <div className="container mx-auto p-4 text-center">
+        <p className="text-gray-600 space-x-2">
+          <span>© {new Date().getFullYear()}</span>
+          <NavLink to="/" className="font-bold">
+            <span className="text-blue-500">rentail</span>
+            .space
+          </NavLink>
+          <NavLink to="/demo">All rights reserved.</NavLink>
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default function App() {
-	return <Outlet />;
+  return <Outlet />;
 }
