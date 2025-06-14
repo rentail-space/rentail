19f3dc89b7c1cf1e5acaf41848d708aacf305e40
Assaf Arkin <assaf@labnotes.org>
2025-06-13 19:31:39 -0700

    Fix: drop top-level await

--- app/lib/llm.ts
+++ app/lib/llm.ts
@@ -6,15 +6,20 @@ import { createOllama } from "ollama-ai-provider";
 let model = createAnthropic({
   apiKey: env.get("ANTHROPIC_API_KEY").required().asString(),
 })("claude-4-opus-20250514");
-try {
-  await new Promise((resolve, reject) =>
-    https
-      .get("https://google.com", { signal: AbortSignal.timeout(1000) }, resolve)
-      .on("error", (error) => reject(error)),
-  );
-} catch (error) {
-  model = createOllama({})("deepseek-v2:latest");
-}
-console.info("LLM model: %s", model.modelId);
+
+https
+  .get(
+    "https://google.com",
+    { signal: AbortSignal.timeout(1000) },
+    (response) => {
+      console.info("LLM model: %s", model.modelId);
+      response.destroy();
+    },
+  )
+  .on("error", (error) => {
+    console.log(error);
+    model = createOllama({})("deepseek-v2:latest");
+    console.error("LLM model: %s", model.modelId);
+  });
 
 export default model;
