2d143fee8c1c761366025c91aeb6de9a7db0dc67
Assaf Arkin <assaf@labnotes.org>
2025-06-22 15:36:48 -0700

    Add colors for logging

--- app/lib/logger.server.ts
+++ app/lib/logger.server.ts
@@ -1,14 +1,27 @@
 import { Logtail } from "@logtail/node";
+import chalk from "chalk";
 
 const logtail = new Logtail("***REMOVED***", {
   endpoint: "https://s1356404.eu-nbg-2.betterstackdata.com",
 });
 
+const colors = {
+  debug: chalk.blue,
+  log: chalk.white,
+  info: chalk.green,
+  warn: chalk.yellow,
+  error: chalk.red,
+};
+
 ["debug", "log", "info", "warn", "error"].forEach((level) => {
   const original = Reflect.get(console, level);
   const logger = Reflect.get(logtail, level);
+  const color = process.stdout.isTTY
+    ? colors[level as keyof typeof colors]
+    : (message: string) => message;
+
   Reflect.set(console, level, (message: string, ...metadata: unknown[]) => {
-    original.call(console, message, ...metadata);
+    original.call(console, color(message), ...metadata);
     logger.call(logtail, message, ...metadata);
   });
 });
