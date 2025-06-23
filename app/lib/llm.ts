45d2a9bf2537c26732c4b848cca36a1426de5e94
Assaf Arkin <assaf@labnotes.org>
2025-06-23 09:42:34 -0700

    Don't fallback on Ollama

--- app/lib/llm.ts
+++ app/lib/llm.ts
@@ -1,26 +1,7 @@
 import { createAnthropic } from "@ai-sdk/anthropic";
 import "dotenv/config";
 import env from "env-var";
-import https from "node:https";
-import { createOllama } from "ollama-ai-provider";
 
-let model = createAnthropic({
+export default createAnthropic({
   apiKey: env.get("ANTHROPIC_API_KEY").required().asString(),
 })("claude-4-opus-20250514");
-
-https
-  .get(
-    "https://google.com",
-    { signal: AbortSignal.timeout(1000) },
-    (response) => {
-      console.info("LLM model: %s", model.modelId);
-      response.destroy();
-    },
-  )
-  .on("error", (error) => {
-    console.log(error);
-    model = createOllama({})("deepseek-v2:latest");
-    console.error("Fallback LLM model: %s", model.modelId);
-  });
-
-export default model;
