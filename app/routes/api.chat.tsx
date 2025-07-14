7b3dec57bdd1a9381dee4a9cb10fbdba1f6587a3
Assaf Arkin <assaf@labnotes.org>
2025-07-14 08:55:39 -0700

    Rename centers -> spaces

--- app/routes/api.chat.tsx
+++ app/routes/api.chat.tsx
@@ -1,19 +1,19 @@
 import { type Message, streamText } from "ai";
 import invariant from "tiny-invariant";
 import model from "~/lib/llm";
-import centers from "../lib/centers.md?raw";
 import general from "../lib/general.md?raw";
+import spaces from "../lib/spaces.md?raw";
 import type { Route } from "./+types/api.chat";
 
 invariant(general, "General prompt is required");
-invariant(centers, "Centers prompt is required");
+invariant(spaces, "Centers list is required");
 
 export async function action({ request }: Route.ActionArgs) {
   const { messages } = (await request.json()) as { messages: Message[] };
   const result = streamText({
     messages,
     model,
-    system: [general, centers].join("\n\n=====\n\n"),
+    system: [general, spaces].join("\n\n=====\n\n"),
   });
   return result.toDataStreamResponse();
 }
