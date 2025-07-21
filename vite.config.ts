ae9f9df9d31ea8261bc75708487024420b122a70
Assaf Arkin <assaf@labnotes.org>
2025-07-21 07:12:53 -0700

    Fix Lint error

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,17 @@ import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const sentryVite =
-  process.env.SENTRY_AUTH_TOKEN &&
-  sentryVitePlugin({
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    org: "labnotes",
-    project: "rentail",
-  });
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
-    ...(process.env.NODE_ENV === "production"
-      ? [sentryVite].filter(Boolean)
-      : [devtoolsJson()]),
+    process.env.NODE_ENV === "production" && process.env.SENTRY_AUTH_TOKEN
+      ? sentryVitePlugin({
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          org: "labnotes",
+          project: "rentail",
+        })
+      : devtoolsJson(),
   ],
 });
