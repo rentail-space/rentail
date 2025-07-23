ba7e4c78b56c1d4e47be077c77bfeda2c596b771
Assaf Arkin <assaf@labnotes.org>
2025-07-22 21:16:20 -0700

    Fix: get started link goes to chat page

--- app/routes/home/HeroSection.tsx
+++ app/routes/home/HeroSection.tsx
@@ -18,7 +18,7 @@ export default function HeroSection({
             </p>
             <div className="flex flex-col sm:flex-row gap-4">
               <a
-                href="#signup"
+                href="/chat"
                 className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition-colors text-center"
               >
                 Get Started
