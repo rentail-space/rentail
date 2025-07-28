dfe95c8b37be6809130bf1dcbc3d0ae6a3828cae
Assaf Arkin <assaf@labnotes.org>
2025-07-28 07:55:49 -0700

    Fixed: user-agent must show first

--- app/routes/robots[.]txt.ts
+++ app/routes/robots[.]txt.ts
@@ -3,10 +3,11 @@ import { generateRobotsTxt } from "@forge42/seo-tools/robots";
 export async function loader() {
   const robotsTxt = generateRobotsTxt([
     {
+      // NOTE: userAgent must show first
+      userAgent: "*",
       allow: ["/"],
       disallow: ["/api/*", "/chat"],
       sitemap: ["https://rentail.space/sitemap.xml"],
-      userAgent: "*",
     },
   ]);
   return new Response(robotsTxt, { headers: { "Content-Type": "text/plain" } });
