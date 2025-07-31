351ccc3e1c5d7fe04a97154868e71354294ccc2e
Assaf Arkin <assaf@labnotes.org>
2025-07-31 15:39:21 -0700

    Log MSW requests

--- test/mocks/server.ts
+++ test/mocks/server.ts
@@ -1,5 +1,20 @@
 import { setupServer } from "msw/node";
+import config from "~/lib/config";
 import { handlers } from "./handlers";
 
 // Setup MSW server with our handlers
 export const server = setupServer(...handlers);
+
+if (config.isDebug)
+  server.events.on("response:mocked", ({ request, response }) =>
+    console.debug(
+      "MSW: %s %s => %s",
+      request.method,
+      request.url,
+      response.status,
+    ),
+  );
+
+server.events.on("request:unhandled", ({ request }) =>
+  console.error("MSW: Unhandled request: %s %s", request.method, request.url),
+);
