88d15848257ccedafeefc5c0edc6dbf9a158c2d5
Assaf Arkin <assaf@labnotes.org>
2025-08-21 17:42:39 -0700

    Using Streamdown

--- app/routes/chat/Messages.tsx
+++ app/routes/chat/Messages.tsx
@@ -1,21 +1,25 @@
-import type { TextUIPart, UIMessage } from "ai";
-import Response from "./Response";
+import type { UIMessage } from "ai";
+import React from "react";
+import type { Components } from "react-markdown";
+import { Streamdown } from "streamdown";
+import { useStickToBottomContext } from "use-stick-to-bottom";
+import askQuestion from "./askQuestion";
 
 export default function Messages({
   className,
   error,
-  setInput,
   isSubmitting: isTyping,
   messages,
   inputRef,
 }: {
   className?: string;
   error?: Error;
-  setInput: (input: string) => void;
   isSubmitting: boolean;
   messages: UIMessage[];
   inputRef: React.RefObject<HTMLInputElement | null>;
 }) {
+  const { scrollToBottom } = useStickToBottomContext();
+
   return (
     <div className={`flex-1 flex flex-col ${className || ""}`}>
       <div className="mx-auto flex max-w-3xl flex-1 flex-col justify-end gap-4 overflow-y-auto p-6">
@@ -24,10 +28,10 @@ export default function Messages({
             <UserMessage key={index.toString()} message={message} />
           ) : (
             <ResponseMessage
-              setInput={setInput}
+              inputRef={inputRef}
               key={index.toString()}
               message={message}
-              inputRef={inputRef}
+              scrollToBottom={scrollToBottom}
             />
           ),
         )}
@@ -54,13 +58,13 @@ function UserMessage({ message }: { message: UIMessage }) {
 }
 
 function ResponseMessage({
-  setInput,
   message,
   inputRef,
+  scrollToBottom,
 }: {
-  setInput: (input: string) => void;
   message: UIMessage;
   inputRef: React.RefObject<HTMLInputElement | null>;
+  scrollToBottom: () => void;
 }) {
   return (
     <div className="chat chat-start">
@@ -73,9 +77,22 @@ function ResponseMessage({
         />
       </div>
       <div className="chat-bubble prose prose-base">
-        <Response setInput={setInput} inputRef={inputRef}>
-          {message.parts.map((part) => (part as TextUIPart).text).join("")}
-        </Response>
+        <div key={message.id}>
+          {message.parts
+            .filter((part) => part.type === "text")
+            .map((part, index) => (
+              <Streamdown
+                components={getComponents({ inputRef, scrollToBottom })}
+                key={index.toString()}
+                rehypePlugins={[]}
+                allowedImagePrefixes={["*"]}
+                allowedLinkPrefixes={["*"]}
+                defaultOrigin="https://rentail.space"
+              >
+                {part.text}
+              </Streamdown>
+            ))}
+        </div>
       </div>
     </div>
   );
@@ -101,4 +118,80 @@ function ErrorNotice({ error }: { error?: Error }) {
       {error.message || "Some error happened"}
     </div>
   ) : null;
+}
+
+function getComponents({
+  inputRef,
+  scrollToBottom,
+}: {
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  scrollToBottom: () => void;
+}): Components {
+  return {
+    ol: ({ node, children, className, ...props }) => (
+      <ol className={"ml-4 list-outside list-decimal"} {...props}>
+        {children}
+      </ol>
+    ),
+    li: ({ node, children, className, ...props }) => (
+      <li className={"py-1"} {...props}>
+        {children}
+      </li>
+    ),
+    ul: ({ node, children, className, ...props }) => (
+      <ul className={"ml-4 list-outside list-disc"} {...props}>
+        {children}
+      </ul>
+    ),
+    strong: ({ node, children, className, ...props }) => (
+      <span className={"font-semibold"} {...props}>
+        {children}
+      </span>
+    ),
+    a: ({ node, children, className, ...props }) => {
+      return (
+        <a
+          className="btn btn-soft btn-primary"
+          href={`?q=${children}`}
+          onClick={async (event) => {
+            event.preventDefault();
+            await askQuestion({
+              question: React.Children.toArray(children),
+              scrollToBottom,
+              inputRef,
+            });
+          }}
+          rel="noreferrer"
+          target="_blank"
+          {...props}
+        >
+          {children}
+        </a>
+      );
+    },
+    button: ({ node, children, className, ...props }) => (
+      <button className="btn btn-soft btn-primary" {...props}>
+        {children}
+      </button>
+    ),
+    h1: ({ node, children, className, ...props }) => (
+      <h1 className={"mt-6 mb-2 font-semibold text-xl"} {...props}>
+        {children}
+      </h1>
+    ),
+    h2: ({ node, children, className, ...props }) => (
+      <h2 className={"mt-6 mb-2 font-semibold text-lg"} {...props}>
+        {children}
+      </h2>
+    ),
+    h3: ({ node, children, className, ...props }) => (
+      <h3 className={"mt-6 mb-2 font-semibold text-md"} {...props}>
+        {children}
+      </h3>
+    ),
+    hr: () => <hr className="border-gray-300" />,
+    p: ({ children }: React.HTMLAttributes<HTMLParagraphElement>) => (
+      <p className="mt-2 mb-2 whitespace-pre-wrap">{children}</p>
+    ),
+  };
 }
