eb7d135a61dfe4384fec481c1962535994236d76
Assaf Arkin <assaf@labnotes.org>
2025-09-04 09:26:41 -0700

    Don't load keys in db push

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,21 @@
-import prisma from "~/lib/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+import env from "env-var";
+import { PrismaClient } from "./generated/client";
+
+// NOTE don't use lib/config here, we don't plan to set all the environment
+// variables just to seed the database.
+
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? // secretlint-disable-next-line
+      "postgresql://postgres:postgres@localhost:5432/postgres"
+    : env.get("DATABASE_URL").required().asUrlString();
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({
+  adapter,
+  errorFormat: "pretty",
+  log: ["error", "warn", "query", "info"],
+});
 
 const user = {
   id: "wxxx3cwnw9o4g6zehqg0dswy",
