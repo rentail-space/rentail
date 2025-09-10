19fbdceb4ac5f9e89774bcdb4584ab737954412c
Assaf Arkin <assaf@labnotes.org>
2025-09-10 16:10:04 -0700

    Drop error page we don't need it any more

--- app/routes/error.tsx
+++
@@ -1,6 +0,0 @@
-export async function loader() {
-  throw new Error("some error thrown in a loader");
-}
-export default function ExamplePage() {
-  return <div>Loading this page will throw an error</div>;
-}
