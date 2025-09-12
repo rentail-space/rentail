1d2b6fcd75ecf926f6c4bbf31805781f25446979
Assaf Arkin <assaf@labnotes.org>
2025-09-12 08:54:41 -0700

    Minor tweak

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
-import {
-  type SentryReactRouterBuildOptions,
-  sentryReactRouter,
-} from "@sentry/react-router";
+import { sentryReactRouter } from "@sentry/react-router";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-const sentryConfig: SentryReactRouterBuildOptions | undefined = process.env
-  .SENTRY_AUTH_TOKEN
-  ? {
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: "labnotes",
-      project: "rentail",
-    }
-  : undefined;
 
 export default defineConfig((config) => ({
   plugins: [
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
-    sentryConfig ? sentryReactRouter(sentryConfig, config) : devtoolsJson(),
+    process.env.SENTRY_AUTH_TOKEN
+      ? sentryReactRouter(
+          {
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+            org: "labnotes",
+            project: "rentail",
+          },
+          config,
+        )
+      : devtoolsJson(),
   ],
   ssr: { noExternal: ["streamdown"] },
 }));
