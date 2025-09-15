c91e4ea665f0f747d8a57618e8b0252a7113e52d
Assaf Arkin <assaf@labnotes.org>
2025-09-15 10:05:44 -0700

    Don't use this script

--- scripts/testing.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-# secretlint-disable-next-line
-export DIRECT_URL=postgresql://postgres:postgres@localhost:5432/postgres
-npx prisma db push
-npx prisma db seed
