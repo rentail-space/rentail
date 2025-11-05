9ebd30feea131371d72acfbbe2b2121a73b9e245
Assaf Arkin <assaf@labnotes.org>
2025-11-04 16:24:12 -0800

    🔧 chore: Enhance property availability checks and JSON structure
    - Improved the `findNearbyProperties` function to filter and return only available properties, increasing search result accuracy.
    - Added an `available` field to JSON data for shopping centers, ensuring uniformity in property status representation.
    - Reformatted image URLs in JSON files for better readability and consistency across all entries.

---
+++ app/routes/properties/route.tsx
@@ -0,0 +1,87 @@
+import { Link } from "@react-email/components";
+import type { PropertySpace } from "prisma/generated/client";
+import type { PropertyGetPayload } from "prisma/generated/models";
+import prisma from "~/lib/prisma";
+
+export async function loader() {
+  const properties = await prisma.property.findMany({
+    include: { spaces: true },
+  });
+  return properties;
+}
+
+export default function PropertyPage({
+  loaderData,
+}: {
+  loaderData: PropertyGetPayload<{ include: { spaces: true } }>[];
+}) {
+  return (
+    <div className="prose prose-2xl mx-auto flex flex-col gap-4">
+      {loaderData.map((property) => (
+        <Property key={property.id} property={property} />
+      ))}
+    </div>
+  );
+}
+
+function Property({
+  property,
+}: {
+  property: PropertyGetPayload<{ include: { spaces: true } }>;
+}) {
+  const paragraphs = property.description.split("\n");
+
+  return (
+    <div key={property.id}>
+      <h2>
+        <Link href={`/property/${property.id}`}>{property.name}</Link>
+      </h2>
+      <Link
+        href={`https://maps.google.com/?q=${encodeURIComponent(`${property.address}, ${property.city}, ${property.state} ${property.country}`)}`}
+      >
+        {property.address}, {property.city}, {property.state}
+      </Link>
+      <details>
+        <summary className="summary-open:hidden">{paragraphs[0]}</summary>
+        {paragraphs.slice(1).map((line, index) => (
+          <p key={index.toString()}>{line}</p>
+        ))}
+      </details>
+
+      <Spaces spaces={property.spaces} />
+    </div>
+  );
+}
+
+function Spaces({ spaces }: { spaces: PropertySpace[] }) {
+  if (spaces.length === 0)
+    return <p className="text-center text-gray-500">No spaces available</p>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Number</th>
+          <th>Size</th>
+          <th>Type</th>
+          <th>Floor</th>
+        </tr>
+      </thead>
+      <tbody>
+        {spaces
+          .sort((a, b) =>
+            a.type !== b.type
+              ? a.type.localeCompare(b.type)
+              : a.number.localeCompare(b.number),
+          )
+          .map((space) => (
+            <tr key={space.id}>
+              <td>{space.number}</td>
+              <td>{space.size} sqft</td>
+              <td>{space.type}</td>
+              <td>{space.floor}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+}
