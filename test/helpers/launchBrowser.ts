24b308e1fa3edf327e12be55b53a89b511cc38fc
Assaf Arkin <assaf@labnotes.org>
2025-11-23 14:12:22 -0800

    refactor(tests): update page navigation to wait for DOM content loading
    - Modified the goto function in launchBrowser.ts to include the waitUntil option set to "domcontentloaded", enhancing page loading behavior and responsiveness during tests.

--- test/helpers/launchBrowser.ts
+++ test/helpers/launchBrowser.ts
@@ -25,7 +25,7 @@ export async function goto(path: string, headers?: HeadersInit): Promise<Page> {
   const context = await newContext();
   const page = await context.newPage();
   await page.setExtraHTTPHeaders(Object.fromEntries(new Headers(headers)));
-  await page.goto(path, { timeout: 5_000 });
+  await page.goto(path, { timeout: 5_000, waitUntil: "domcontentloaded" });
   return page;
 }
 
