fa6d9418e9b097b2e7073f1906a0d4341e964cb9
Assaf Arkin <assaf@labnotes.org>
2025-11-25 10:46:45 -0800

    feat(user-info): add UTM information display in UserInfoCard component
    - Introduced a new row in the UserInfoCard to display UTM parameters associated with the user.
    - Enhanced the user information presentation for better analytics tracking and visibility.

--- app/routes/admin.user.$userId.tsx
+++ app/routes/admin.user.$userId.tsx
@@ -93,6 +93,12 @@ function UserInfoCard({ user }: { user: User }) {
             </td>
           </tr>
           <tr>
+            <th className="align-middle">UTM</th>
+            <td className="truncate align-middle" title={user.utm as string}>
+              {user.utm as string}
+            </td>
+          </tr>
+          <tr>
             <th className="align-middle">Location</th>
             <td
               className="truncate align-middle"
