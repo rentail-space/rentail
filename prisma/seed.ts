1e841a3975ce860bbe6dc6bb4151e59972134647
Assaf Arkin <assaf@labnotes.org>
2025-12-10 07:54:31 -0800

    refactor(scrape.yml, seed.ts): streamline database seeding process and enhance logging
    - Updated the scrape.yml workflow to include the DOPPLER_TOKEN environment variable for the database seeding step.
    - Modified the seed command to run both the Prisma generation and seeding in a single step for efficiency.
    - Added a console log in seed.ts to output the DATABASE_URL during the seeding process for better debugging visibility.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,5 +2,6 @@ import debug from "debug";
 import seedCenters from "./seed/seedCenters";
 
 debug.enable("seed");
+console.log("Seeding database: %s", process.env.DATABASE_URL);
 await seedCenters();
 process.exit(0);
