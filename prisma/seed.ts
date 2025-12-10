3441df32e7180f3b8c47bd991ac9dce76aa1de69
Assaf Arkin <assaf@labnotes.org>
2025-12-10 08:19:24 -0800

    refactor(scrape.yml, seed.ts): enhance workflow and streamline seeding process
    - Updated the scrape.yml workflow to remove redundant steps for installing the Doppler CLI and setting up Biome, consolidating the process for better efficiency.
    - Removed the console log in seed.ts for a cleaner output during the database seeding process.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,6 +2,5 @@ import debug from "debug";
 import seedCenters from "./seed/seedCenters";
 
 debug.enable("seed");
-console.log("Seeding database: %s", process.env.DATABASE_URL);
 await seedCenters();
 process.exit(0);
