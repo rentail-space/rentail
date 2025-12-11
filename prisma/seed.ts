68bc6132004fb942d71708396b72c6c9194ed3c3
Assaf Arkin <assaf@labnotes.org>
2025-12-11 09:49:36 -0800

    fix(package.json, seed.ts): add missing newline and remove unused Prisma import
    - Added a newline at the end of package.json to comply with formatting standards.
    - Removed the unused Prisma import in seed.ts to clean up the code and improve clarity.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
 import debug from "debug";
-import prisma from "~/lib/prisma";
 import seedCenters from "./seed/seedCenters";
 
 debug.enable("seed");
 await seedCenters();
-await prisma.$disconnect();
