7bc8f236274f69b00216efbe2e0e9b5061a30ad5
Assaf Arkin <assaf@labnotes.org>
2025-12-12 09:52:48 -0800

    refactor(scrape.yml, prisma.ts): update database seeding command and enhance SSL configuration
    - Changed the database seeding command in the scrape.yml workflow to use `pnpm prisma db seed` for improved clarity and functionality.
    - Enhanced the pg Pool configuration in prisma.ts to include SSL settings, ensuring secure connections with the added certificate for production environments.
    - Introduced a new certificate file for SSL configuration to support secure database connections.

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -2,6 +2,8 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import debug from "debug";
 import dotenv from "dotenv";
 import { invariant } from "es-toolkit";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
 import pg from "pg";
 import { PrismaClient } from "prisma/generated/client";
 
@@ -15,6 +17,10 @@ const pool = new pg.Pool({
   idleTimeoutMillis: 0,
   connectionTimeoutMillis: 0,
   allowExitOnIdle: true,
+  ssl: {
+    rejectUnauthorized: false,
+    cert: readFileSync(resolve("prisma/prod-ca-2021.crt")),
+  },
 });
 
 export default new PrismaClient({
