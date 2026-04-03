3717d07a1defc312bcc74da34ee5767c1719b9d2
Assaf Arkin <assaf@labnotes.org>
2026-04-03 12:25:59 -0700

    🔧 chore(env): update dotenv configuration to allow overriding environment variables

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import env from "env-var";
 
-dotenv.configDotenv({ quiet: true });
+dotenv.config({ quiet: true, override: true });
 
 const envVars = {
   isProduction: process.env.NODE_ENV === "production",
